e0fd18422685fda00c37f86311b0d1ae70ebc477
unknown <ra99829@uem.br>
2019-07-30 19:11:16 -0300

    alo

--- processador.c
+++ processador.c
@@ -1,8 +1,9 @@
 #include "processador.h"
+#include "ula.h"
 
 void estagio_busca_pipeline(){
     IR.valor = read(PC.valor);    
-    PC.valor += 4; //tem que fazer essa adição através da ULA, mas a ula nao ta pronta kkkj
+    PC.valor += ula_somador(PC.valor, 4); //tem que fazer essa adição através da ULA, mas a ula nao ta pronta kkkj
 }
 
 //decodificacao do codigo da instrucao no IR, e coloca na fila
---
+++ unidade_func.c
@@ -0,0 +1,18 @@
+unidade_func* init_unidade_func(){
+
+}
+int issue(){
+
+}
+
+void read_operands(){
+
+}
+
+void execute(){
+
+}
+
+void write_back(){
+
+}
